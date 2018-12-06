cc2c1a000ac4b6202fb0005cf490d5315ed3609e
Ben J. Ward <ward9250@gmail.com>
2018-12-06 00:25:20 +0000

    Add coverage folder and script

---
+++ coverage/coverage.jl
@@ -0,0 +1,9 @@
+# Only run coverage from linux nightly build on travis.
+get(ENV, "TRAVIS_OS_NAME", "")       == "linux"   || exit()
+get(ENV, "TRAVIS_JULIA_VERSION", "") == "1.0" || exit()
+
+using Coverage
+
+cd(joinpath(@__DIR__, "..")) do
+    Codecov.submit(Codecov.process_folder())
+end
