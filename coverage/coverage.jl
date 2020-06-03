b178203fd6249c8c5d663394d44553bbb705ddd6
Ben J. Ward <benjward@protonmail.com>
2020-06-03 02:16:29 +0100

    Edit files for LTS

--- coverage/coverage.jl
+++
@@ -1,9 +0,0 @@
-# Only run coverage from linux nightly build on travis.
-get(ENV, "TRAVIS_OS_NAME", "")       == "linux"   || exit()
-get(ENV, "TRAVIS_JULIA_VERSION", "") == "1.1" || exit()
-
-using Coverage
-
-cd(joinpath(@__DIR__, "..")) do
-    Codecov.submit(Codecov.process_folder())
-end
