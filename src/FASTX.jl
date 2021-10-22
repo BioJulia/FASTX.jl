fba10715a74c184071074a136a7071d7e92de682
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2021-10-22 10:11:45 +0200

    Export all user-facing functions

--- src/FASTX.jl
+++ src/FASTX.jl
@@ -4,20 +4,30 @@ export
     FASTA,
     FASTQ,
     identifier,
+    hasidentifier,
     description,
+    hasdescription,
+    header,
     sequence,
-    quality
+    hassequence,
+    quality,
+    hasquality,
+    quality_iter
 
 # Generic methods
 function identifier end
 function description end
 function sequence end
+function hasidentifier end
+function hasdescription end
+function hassequence end
+function header end
 
 include("fasta/fasta.jl")
 include("fastq/fastq.jl")
 
 import .FASTA
 import .FASTQ
-import .FASTQ: quality
+import .FASTQ: quality, hasquality, quality_iter
 
 end # module
